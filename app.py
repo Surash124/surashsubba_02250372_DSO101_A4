5fb71a074198d1e8866bef17162f67fd2d9385c6
Surash124 <surashu.124@gmail.com>
2026-05-10 00:50:23 +0600

    changed the port number as 127.0.0.1 wont work in render

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from logging.config import listen
+
 from flask import Flask        # import Flask library
 app = Flask(__name__)          # create the app
 
@@ -6,4 +8,6 @@ def home():
     return "Hello DevOps!"     # return this text
 
 if __name__ == '__main__':
-    app.run()                  # start the server
+    app.run(host='0.0.0.0', port=5000)  # start the server
+                                        #listen to ALL incoming connections
+                                        # on port 5000
